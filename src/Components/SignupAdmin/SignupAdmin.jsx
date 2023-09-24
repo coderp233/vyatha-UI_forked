fcab9a1b450e0c2694b6c5b016d4db33a35099bf
Priyanka <mandirakumari1538@gmail.com>
2023-09-24 10:51:28 +0530

    Modified

--- src/Components/SignupAdmin/SignupAdmin.jsx
+++ src/Components/SignupAdmin/SignupAdmin.jsx
@@ -1,5 +1,5 @@
-import React,{useState} from 'react';
-import styles from './SignupAdmin.scss'
+import React from 'react';
+import './SignupAdmin.scss'
 
 
 const SignupAdmin=()=>{
@@ -11,18 +11,18 @@ const SignupAdmin=()=>{
         <form>
           <div className="container">
       <div className="input-group">
-        <input type="text" required="required" className="input"/>
-        <label className="Placeholder">Name</label>
+        <input id='control' type="text" required="required" className="input"/>
+        <label for="control" className="Placeholder">Name</label>
       </div>
       <br/>
       <div className="input-group">
-        <input type="email" className="input" required="required" />
-        <label className="Placeholder">Email</label>
+        <input id='control1' type="email" className="input" required="required" />
+        <label for="control1" className="Placeholder">Email</label>
       </div>
       
       <div className="input-group" >
-      <label for="Designation" className="input" placeholder="Designation"></label>
-  <select name="design" className="select_step" style={{width:'326px', height:'40px',position: "relative", top: "-22px"}} >
+      <label for="Designation1" className="input" placeholder="Designation"></label>
+  <select id='Designation1' name="design" className="select_step" style={{width:'326px', height:'40px',position: "relative", top: "-22px"}} >
   <option value="select" className="Placeholder" >Designation</option>
   <option value="supervisor">Supervisor</option>
   <option value="warden">Warden</option>
@@ -31,18 +31,18 @@ const SignupAdmin=()=>{
       </div>
   
       <div className="input-group">
-        <input type="number" className="input" required="reqired" />
-        <label className="Placeholder">Phone</label>
+        <input id='control2' type="number" className="input" required="reqired" />
+        <label for="control2" className="Placeholder">Phone</label>
       </div>
       <br/>
       <div className="input-group">
-        <input type="password" className="input" required="required"/>
-        <label className="Placeholder">Password</label>
+        <input id='control3' type="password" className="input" required="required"/>
+        <label for="control3" className="Placeholder">Password</label>
       </div>
       <br/>
       <div className="input-group">
-        <input type="password" className="input" required="required" />
-        <label className="Placeholder">Confirm Password</label>
+        <input id='control4' type="password" className="input" required="required" />
+        <label for="control4" className="Placeholder">Confirm Password</label>
       </div>
       <br/></div>
       <div className="submit-btn">
